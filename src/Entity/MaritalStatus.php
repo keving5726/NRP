c880587316d49f648c3826856697a19e9bbde9e3
Kevin Gonzalez <keving5726@gmail.com>
2019-07-16 14:17:13 -0400

    Creating MaritalStatus Entity

---
+++ src/Entity/MaritalStatus.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MaritalStatusRepository")
+ */
+class MaritalStatus
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30, unique=true)
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->status;
+    }
+}
