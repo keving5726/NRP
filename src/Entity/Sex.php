a6ba1fc5aa30ed22b8012d670cef33a62a2f9a70
Kevin Gonzalez <keving5726@gmail.com>
2019-07-16 14:14:32 -0400

    Creating Sex Entity

---
+++ src/Entity/Sex.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SexRepository")
+ */
+class Sex
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=20, unique=true)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->type;
+    }
+}
