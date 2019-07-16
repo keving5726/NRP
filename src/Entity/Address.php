8ae7d6af990c908e62ba76f396be972c22c8a72e
Kevin Gonzalez <keving5726@gmail.com>
2019-07-16 14:36:22 -0400

    Creating Address Entity

---
+++ src/Entity/Address.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $userIdentification;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $postcode;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserIdentification(): ?User
+    {
+        return $this->userIdentification;
+    }
+
+    public function setUserIdentification(User $userIdentification): self
+    {
+        $this->userIdentification = $userIdentification;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getPostcode(): ?int
+    {
+        return $this->postcode;
+    }
+
+    public function setPostcode(int $postcode): self
+    {
+        $this->postcode = $postcode;
+
+        return $this;
+    }
+}
