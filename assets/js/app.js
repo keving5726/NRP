89003c81421e8be4b2b4a175a8eecc6106955cf6
Kevin Gonzalez <keving5726@gmail.com>
2019-08-15 14:57:23 -0400

    Installing FOSJsRoutingBundle

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,5 +16,3 @@ require('bootstrap');
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('fontawesome', './assets/js/fontawesome.js')
+    .addEntry('ajax', './assets/js/ajax.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
